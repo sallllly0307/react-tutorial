057d0519ce768ae96954e47ec7c2d9bc762a51d9
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-01-21 23:43:37 +0900

    Update to remember clicked area

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          value: null,
+        };
+    }
     render() {
       return (
-        <button className="square" onClick={() => {alert('click');}}> 
-          {this.props.value} 
+        <button 
+          className="square" 
+          onClick={() => this.setState({value: 'X'})}
+        > 
+          {this.state.value} 
         </button>
         //クリックされたらXを表示したいので、on click プロパティに関数を渡すようにする
         //Square の render メソッドで、渡された値を表示するよう書き換える
