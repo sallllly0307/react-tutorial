adc37ff5924a189e7d42f1f0d6c531da1ce3a7a2
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-01-23 01:54:34 +0900

    Implemnt judge winner

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ function Square(props) {
         //Square の render メソッドで、渡された値を表示するよう書き換える
       );
     }
-  }
   
   class Board extends React.Component {
     constructor(props) {
@@ -45,7 +44,7 @@ function Square(props) {
     render() {
       const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       //どちらのプレイヤーの手番なのかを表示する
-      
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -91,4 +90,23 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
