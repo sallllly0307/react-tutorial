d16e035148484296db0aff9f4c70ce3aa0798703
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-01-22 23:11:52 +0900

    update square to use props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-    render() {
+function Square(props) {
       return (
         <button 
           className="square" 
