8d4678c061ac16d5207fe44f69c63da21ea2b025
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-01-22 21:46:40 +0900

    delete constructor of Square

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          value: null,
-        };
-    }
     render() {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'})}
+          onClick={() => this.props.onClick()}
         > 
-          {this.state.value} 
+          {this.props.value} 
         </button>
         //クリックされたらXを表示したいので、on click プロパティに関数を渡すようにする
         //Square の render メソッドで、渡された値を表示するよう書き換える
