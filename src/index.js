3f62edd86c78134de8fd36f70120d1fabdb20522
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-01-24 16:25:53 +0900

    add status to game info

--- src/index.js
+++ src/index.js
@@ -93,11 +93,11 @@ function Square(props) {
           <div className="game-board">
             <Board 
               squares={current.squares}
-              onClick={(i)=>this.handleClick(i)}
+              onClick={(i) => this.handleClick(i)}
             />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
