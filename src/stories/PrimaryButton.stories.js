80fa87dc800c5a3699b6d4c7a645d6d048a86a39
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-02-14 23:57:12 +0900

    Fix Button Stories

--- src/stories/PrimaryButton.stories.js
+++ src/stories/PrimaryButton.stories.js
@@ -1,3 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './button.css';
+import './primarybutton.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: 'PrimaryButton',
+    component: 'primarybutton',
+    argTypes: {
+      backgroundColor: { control: 'color' },
+    },
+  };
+  
+  // eslint-disable-next-line react/jsx-no-undef
+  const Template = (args) => <Button {...args} />;
+  
+  export const Primary = Template.bind({});
+  Primary.args = {
+    primary: true,
+    label: 'Button',
+  };
+  
+  export const Secondary = Template.bind({});
+  Secondary.args = {
+    label: 'Button',
+  };
