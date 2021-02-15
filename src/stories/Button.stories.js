16f6046e86a4c6143e7eb3a832252be26813c9a4
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-02-15 23:15:24 +0900

    Fix Primary Button stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,36 +1,21 @@
 import React from 'react';
-
-import { Button } from './Button';
+import './button.css';
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-};
-
-const Template = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+    title: 'Button',
+    component: 'button',
+  };
+  
+  const Template = (args) => <button {...args} />;
+  
+  export const Primary = Template.bind({});
+  Primary.args = {
+    primary: true,
+    label: 'Go to game start',
+  };
+  
+  export const Secondary = Template.bind({});
+  Secondary.args = {
+    primary: false,
+    label: 'Go to move #',
+  };
