cb17a701887ca1539398ae4b2cb0876d87d5eb8c
sallllly0307 <sara.love.choco.0307@gmail.com>
2021-02-20 23:55:55 +0900

    Edit square style

---
+++ src/stories/square.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Button } from './Button';
+import './square.css';
---
+++ src/stories/square.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Square } from './Square';
+
+export default {
+    title: 'Square',
+    component: Square,
+  };
